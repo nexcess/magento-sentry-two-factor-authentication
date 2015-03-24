10dbcc950b9d6ae4c31d68306401be44a166daef
g-milligan <gmilligan@human-element.com>
2015-03-23 21:16:32 -0400

    dynamic show hide duo panel based on selected provider

---
+++ js/he_twofactor/adminpanel.js
@@ -0,0 +1,43 @@
+//jquery substitute for document ready (no guarantee that either jquery OR prototype will be available)
+(function(g,b){function c(){if(!e){e=!0;for(var a=0;a<d.length;a++)d[a].fn.call(window,d[a].ctx);d=[]}}function h(){"complete"===document.readyState&&c()}b=b||window;var d=[],e=!1,f=!1;b[g||"he_twofactor_ready"]=function(a,b){e?setTimeout(function(){a(b)},1):(d.push({fn:a,ctx:b}),"complete"===document.readyState?setTimeout(c,1):f||(document.addEventListener?(document.addEventListener("DOMContentLoaded",c,!1),window.addEventListener("load",c,!1)):(document.attachEvent("onreadystatechange",h),window.attachEvent("onload",
+c)),f=!0))}})("he_twofactor_ready",window);
+//on document ready
+he_twofactor_ready(function() {
+  //if the provider selector is on this page
+  var providerSelect=document.getElementById('he2faconfig_control_provider');
+  if(providerSelect!=undefined){
+    //if the "duo" section is on this page
+    var duoHeadLink=document.getElementById('he2faconfig_duo-head');
+    if(duoHeadLink!=undefined){
+      var duoPanel=duoHeadLink.parentNode.parentNode;
+      //set the duo panel's visibility
+      var duoPanelVisibility=function(selectValue){
+        //if duo is selected
+        if(selectValue=='duo'){
+          //show the panel
+          duoPanel.style.display='block';
+          //if this browser supports classList.contains
+          if(duoPanel.classList){
+            if(duoPanel.classList.contains){
+              //if the class list does NOT contain active
+              if(!duoPanel.classList.contains('active')){
+                //open the panel
+                duoHeadLink.onclick();
+              }
+            }
+          }
+        }else{
+          //duo is NOT selected... hide the panel
+          duoPanel.style.display='none';
+        }
+      };
+      //selected provider change event
+      providerSelect.onchange=function(){
+        //set the duo panel's visibility
+        duoPanelVisibility(this.value);
+      };
+      //set the duo panel's visibility on page load
+      duoPanelVisibility(providerSelect.value);
+    }
+  }
+});
